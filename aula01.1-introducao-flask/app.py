e0744a69fbafacf2d07c281664e342fd8e21282f
DSM4 <fatec-dsm4@academico.intra>
2025-08-25 19:49:54 -0300

    create routes.py

--- aula01.1-introducao-flask/app.py
+++ aula01.1-introducao-flask/app.py
@@ -7,27 +7,6 @@ app = Flask(__name__, template_folder='views')
 # Rota principal
 
 
-@app.route('/')
-def home():  # funcao que sera executada pela rota
-    return render_template('index.html')
-
-
-@app.route('/games')
-def games():
-    title = 'The Witcher 3: Wild Hunt'
-    year = 2015
-    category = 'RPG'
-    # array em python
-    players = ['arthur', 'amanda']
-    # dicionario em python
-    console = {'name': 'Playstation 5', 'maker': 'Sony', 'year': 2020}
-    return render_template('games.html',
-                           title=title,
-                           year=year,
-                           category=category,
-                           players=players, 
-                           console=console)
-
 
 # se for executado diretamente pelo interpretador
 if __name__ == '__main__':
